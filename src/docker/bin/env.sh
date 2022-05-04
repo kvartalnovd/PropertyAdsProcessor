8955a7e4d1726c0a56798b35a97ce5c0893e3547
Даниил Квартальнов <daniil.kwarto@yandex.ru>
2022-05-04 21:25:04 +0300

    init project

---
+++ src/docker/bin/env.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
+cd $(dirname ${SCRIPT_DIR}/../..)
+
+export $(cat ${SCRIPT_DIR}/../.env | xargs)
+
+DOCKER_FILE="-f docker-compose.yml"
+COMPOSE="docker-compose ${DOCKER_FILE}"
