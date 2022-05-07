48c2d9a1569d9239c8563401b2da8695b4d66dd0
Даниил Квартальнов <daniil.kwarto@yandex.ru>
2022-05-07 13:48:52 +0300

    update environment startup script (environment.sh) & temporarily added an adminer image

--- src/docker/bin/env.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
-cd $(dirname ${SCRIPT_DIR}/../..)  # /src/docker/
-
-export $(cat .env.dev | xargs)
-
-DOCKER_FILE="docker-compose.yml"
-COMPOSE="docker-compose -f ${DOCKER_FILE}"
---
+++ src/docker/bin/environment.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Throws error - if it is.
+#
+# @example: exitIfError $? "Your error text".
+# @example: exitIfError $1 "Your error text".
+function exitIfError() {
+  local exit_code=$1
+  shift
+  [[ $exit_code ]] &&
+    ((exit_code != 0)) && {
+      echo "ERROR. $@" >&2;
+      exit "$exit_code";
+    }
+}
+
+
+# Loads environment's arguments from dotenv (.env) files
+#
+# @example: loadDotenvArgs ".env.dev"
+function loadDotenvArgs() {
+    if [ ! -f ${ENVIRONMENT_FILE} ]; then
+      exitIfError 0 "The environment file ${ENVIRONMENT_FILE} was not found in $(pwd)";
+    fi
+    echo $(cat $1 | grep -v '#' | sed 's/\r$//' | awk '/=/ {print $1}');
+}
+
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )";
+cd $(dirname ${SCRIPT_DIR}/../..);  # /src/docker/
+
+ENVIRONMENT_FILE=".env";
+if [ ! -f ${ENVIRONMENT_FILE} ]; then
+  # Check: If there is no production environment file, then we get a development environment file
+  ENVIRONMENT_FILE=".env.dev";
+fi
+
+
+# Pulling out environment variables
+export $(loadDotenvArgs "${ENVIRONMENT_FILE}");
+
+DOCKER_FILE="docker-compose.yml"
+COMPOSE="docker-compose -f ${DOCKER_FILE}"
