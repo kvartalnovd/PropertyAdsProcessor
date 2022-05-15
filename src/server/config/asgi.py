cfc69bd5472fdf3af4f30c83839c629de9eb3194
Даниил Квартальнов <daniil.kwarto@yandex.ru>
2022-05-15 18:34:41 +0300

    fix asgi

--- src/server/config/asgi.py
+++ src/server/config/asgi.py
@@ -6,18 +6,20 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 import os
 
 from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from components.common.routes import websocket_urlpatterns
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django_asgi_app = get_asgi_application()
+
+from components.common import ws_routes
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
+    "http": django_asgi_app,
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            ws_routes.urlpatterns
         )
     )
 })
